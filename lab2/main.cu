254fcb16fb9568b6b6c3bead7e91851a4eb44c47
Feresey <p.milko1999@yandex.ru>
2020-11-08 22:43:09 +0300

    lab2: remove redundant functions

--- lab2/main.cu
+++ lab2/main.cu
@@ -2,31 +2,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-#define CHECK(SIZE_EQ, WANT)    \
-    {                           \
-        size_t err = (SIZE_EQ); \
-        if (err != WANT) {      \
-            return err;         \
-        };                      \
-    }
-
-int read_image(FILE* f, uint32_t** out, uint32_t* w, uint32_t* h)
-{
-    CHECK(fread(w, sizeof(uint32_t), 1, f), 1);
-    CHECK(fread(h, sizeof(uint32_t), 1, f), 1);
-    const size_t size = *w * *h;
-    *out = (uint32_t*)malloc(size * sizeof(uint32_t));
-    CHECK(fread(*out, sizeof(uint32_t), size, f), size);
-    return 0;
-}
-
-int write_image(FILE* f, uint32_t* data, uint32_t w, uint32_t h)
-{
-    CHECK(fwrite(&w, sizeof(uint32_t), 1, f), 1);
-    CHECK(fwrite(&h, sizeof(uint32_t), 1, f), 1);
-    CHECK(fwrite(data, sizeof(uint32_t), w * h, f), w * h);
-    return 0;
-}
+#include "serialize.h"
 
 #define CSC(call)                                              \
     do {                                                       \
