5e3ab336cdcbc2bb8ca05aef82c4d7fd748b96b9
Feresey <p.milko1999@yandex.ru>
2021-03-11 22:04:59 +0300

    fix

--- lab7/src/deps.mk
+++ lab7/src/deps.mk
@@ -1,13 +1,7 @@
 build/common.cpp.o: common.cpp dim3/dim3.hpp grid/grid.hpp helpers.hpp helpers.hpp problem.hpp solver.hpp
 build/dim3__dim3.cpp.o: dim3/dim3.cpp dim3/dim3.hpp helpers.hpp
-dim3__dim3.hpp: dim3/dim3.hpp helpers.hpp
 build/exchange.cpp.o: dim3/dim3.hpp exchange.cpp exchange.hpp grid/grid.hpp helpers.hpp problem.hpp
-exchange.hpp: dim3/dim3.hpp grid/grid.hpp helpers.hpp problem.hpp
 build/grid__grid.cpp.o: dim3/dim3.hpp grid/grid.cpp grid/grid.hpp helpers.hpp
-grid__grid.hpp: dim3/dim3.hpp grid/grid.hpp helpers.hpp
-helpers.hpp: 
 build/main.cpp.o: dim3/dim3.hpp grid/grid.hpp helpers.hpp helpers.hpp main.cpp problem.hpp solver.hpp
 build/problem.cpp.o: dim3/dim3.hpp grid/grid.hpp helpers.hpp problem.cpp problem.hpp
-problem.hpp: dim3/dim3.hpp grid/grid.hpp helpers.hpp
-build/solver.cpp.o: dim3/dim3.hpp grid/grid.hpp helpers.hpp helpers.hpp problem.hpp solver.cpp solver.hpp
-solver.hpp: dim3/dim3.hpp grid/grid.hpp helpers.hpp problem.hpp
+build/solver.cpp.o: dim3/dim3.hpp exchange.hpp grid/grid.hpp helpers.hpp helpers.hpp problem.hpp solver.cpp solver.hpp
